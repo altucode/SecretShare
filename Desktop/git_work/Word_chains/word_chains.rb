92ee4e6bdb508e8c50459c8cda4e10429febc92e
App Academy Student <student@appacademy.io>
2014-07-13 21:06:12 -0400

    Phase I completed.

--- Desktop/git_work/Word_chains/word_chains.rb
+++ Desktop/git_work/Word_chains/word_chains.rb
@@ -15,9 +15,44 @@ class WordChainer
       end
       adj_set.add(dict_word) if letter_wrong < 2 && dict_word.length == word.length
     end
-    p adj_set
+    adj_set.delete(word)
     end
+
+    def run(source)
+      @current_words = [source]
+      @all_seen_words = [source]
+
+      while !@current_words.empty?
+        explore_current_words
+      end
+      @all_see_words
+    end
+
+    def explore_current_words
+
+      @current_words.each do |word|
+
+        new_current_words = []
+        adjacent_words(word).each do |inner_word|
+
+          if @all_seen_words.include?(inner_word)
+            next
+          else
+
+            new_current_words << inner_word
+            @all_seen_words << inner_word
+
+          end
+
+        end
+        p @current_words = new_current_words
+      end
+
+    end
+
   end
+
+
 
 
 
@@ -27,4 +62,4 @@ class WordChainer
 
 dict = File.readlines("/Users/appacademy/Desktop/git_work/Hangman/dictionary.txt").map {|word| word.chomp}
 wordchain = WordChainer.new(dict)
-wordchain.adjacent_words("duck")
+p wordchain.run("duck")
